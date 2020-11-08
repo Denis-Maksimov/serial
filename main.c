eae5218d1275719a6a8c0fcd9c54b45563ccc90f
Maksimov <dn.maksimow@gmail.com>
2020-11-08 17:29:06 +0300

    Перевёл на мультиплексный ввод-вывод, снёс байтоёбские очередями

--- main.c
+++ main.c
@@ -1,30 +1,21 @@
 //#include <stdlib.h>
 #include "Serial.h"
-#include <stdio.h>   /* Стандартные объявления ввода/вывода */
-#include <stdlib.h>
+
+
+
+
+
+
 
 int main(){
 
-    char buf[150];
-    
-    struct Serial* COM_port=serial_begin(SERIAL_DEVICE, 9600, mode_8N1);
-    int count;
+    Serial COM_port;
+    serial_begin(&COM_port, SERIAL_DEVICE, 9600, mode_8N1);
     puts("start");
-    int limit=20;
-    while (limit>0)
-    {
-        count = serial_read(COM_port, buf, 150);
-        if(count)
-        {
-            printf(buf);
-            fwrite(buf,1,count,stdout);
-            printf("\n");
-            limit--;
-            serial_write(COM_port,"hello",sizeof("hello"));
-        }
-        
-    }
-    serial_close(COM_port);
+
+    serial_work(&COM_port);
+
+    serial_close(&COM_port);
 
 }
 
